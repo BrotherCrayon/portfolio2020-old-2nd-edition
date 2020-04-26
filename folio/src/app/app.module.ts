6c3406e87bfcd1790f02dfc1bfe11cfbeb27c53c
jake fellows <jake.fellows@hotmail.com>
2020-04-26 22:18:08 +0100

    added cards

--- folio/src/app/app.module.ts
+++ folio/src/app/app.module.ts
@@ -6,17 +6,28 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardsViewComponent } from './components/cards-view/cards-view.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+
+import {MatMenuModule} from '@angular/material/menu';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    CardsViewComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
