f8375bdf6e9229a48248b24cac3bb589e2281bc1
jake fellows <jake.fellows@hotmail.com>
2020-04-28 13:57:02 +0100

    Card update and initial carousel

--- folio/src/app/components/cards-view/cards-view.component.ts
+++ folio/src/app/components/cards-view/cards-view.component.ts
@@ -12,4 +12,46 @@ export class CardsViewComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  cards = [
+    {
+      title: 'Project One',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Show me',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+    {
+      title: 'Project Two',
+      description: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. '
+      + 'A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally '
+      + 'bred for hunting.',
+      buttonText: 'Show me',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+    {
+      title: 'Project Title Three',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Show me',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+    {
+      title: 'Project Title Four',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Show me',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+    {
+      title: 'Project Title Five',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Show me',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+    {
+      title: 'Project Title Six',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Show me',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+    
+  ];
+
 }
