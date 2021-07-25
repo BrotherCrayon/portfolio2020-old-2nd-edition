300c692c9f5dd2f8745a53f6afd223a709f11f41
jake fellows <jake.fellows@hotmail.com>
2021-07-25 22:34:42 +0100

    initial route animation feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { routeAnimation } from './animations/route-animation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    routeAnimation
+  ]
 })
 export class AppComponent {
   title = 'jake fellows';
+
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
+  }
 }
